aaa3162c03a32f2ce51cb138bed57a789cfda7a8
Mukesh <muk142005@gmail.com>
2024-10-16 20:55:04 +0530

    First Commit

--- online_quiz_system/src/main/java/com/demo/online_quiz_system/Exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.demo.online_quiz_system.Exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-	public ResourceNotFoundException(String message) {
-	super(message);
-	}
-
-}
